a162524f51937a73323d81dde18499362d52caaa
bhanusrigoda452 <134681166+bhanusrigoda452@users.noreply.github.com>
2023-05-29 17:58:28 +0530

    upload hello code

---
+++ HelloCode.java
@@ -0,0 +1,13 @@
+public class HelloCode {
+	
+	public static void main(String[] args) {
+
+		System.out.println("Hello Java Git");
+		  
+		  
+	
+
+	}
+
+}
+//https://github.com/bhanusrigoda452/github-web-1.git//
